699e598bb590b3035249781d1a424e2b13c854d8
Alexey Golub <Tyrrrrrr@gmail.com>
2017-09-12 22:16:06 +0300

    Add project files.

---
+++ Readme.md
@@ -0,0 +1,22 @@
+# Tyrrrz.AspNetCore.Mvc.Clicky
+
+Tag helper used to render [Clicky](https://clicky.com) activity tracker in ASP.net Core MVC views.
+
+## Download
+
+- Using nuget: `Install-Package Tyrrrz.AspNetCore.Mvc.Clicky`
+
+## Usage
+
+Make the tag helper available with the `addTagHelper` directive either in your view or `_ViewImports.cshtml`.
+
+```
+@addTagHelper *, Tyrrrz.AspNetCore.Mvc.Clicky
+```
+
+Use the tag helper to render the tracker.
+You will need to specify the `site-id` attribute, which you can find in site preferences on your Clicky dashboard.
+
+```html
+<clicky site-id="075468937" />
+```
---
+++ Tyrrrz.AspNetCore.Mvc.Clicky/TagHelpers/DisqusTagHelper.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.IO;
+using System.Reflection;
+using System.Resources;
+using Microsoft.AspNetCore.Razor.TagHelpers;
+
+namespace Tyrrrz.AspNetCore.Mvc.Clicky.TagHelpers
+{
+    /// <summary>
+    /// Tag helper used to render Clicky tracker
+    /// </summary>
+    public partial class ClickyTagHelper : TagHelper
+    {
+        /// <summary>
+        /// Whether the tag helper is enabled
+        /// </summary>
+        [HtmlAttributeName("enabled")]
+        public bool Enabled { get; set; } = true;
+
+        /// <summary>
+        /// Site short name
+        /// </summary>
+        [HtmlAttributeName("site-id")]
+        public string SiteId { get; set; }
+
+        /// <inheritdoc />
+        public override void Process(TagHelperContext context, TagHelperOutput output)
+        {
+            if (context == null)
+                throw new ArgumentNullException(nameof(context));
+            if (output == null)
+                throw new ArgumentNullException(nameof(output));
+
+            // Validate attributes
+            if (string.IsNullOrWhiteSpace(SiteId))
+                throw new ArgumentException("SiteId attribute must be set");
+
+            // Return if not enabled
+            if (!Enabled)
+            {
+                output.TagName = null;
+                return;
+            }
+
+            // Format the content
+            var content = TemplateHtml;
+            content = content.Replace("__SiteId__", SiteId);
+
+            // Output
+            output.TagName = null;
+            output.Content.SetHtmlContent(content);
+
+            base.Process(context, output);
+        }
+    }
+
+    public partial class ClickyTagHelper
+    {
+        private static readonly string TemplateHtml;
+
+        static ClickyTagHelper()
+        {
+            TemplateHtml = GetTemplateHtml();
+        }
+
+        private static string GetTemplateHtml()
+        {
+            const string resourcePath = "Tyrrrz.AspNetCore.Mvc.Clicky.Resources.Template.html";
+
+            var assembly = typeof(ClickyTagHelper).GetTypeInfo().Assembly;
+            var stream = assembly.GetManifestResourceStream(resourcePath);
+            if (stream == null)
+                throw new MissingManifestResourceException("Could not find template resource");
+
+            using (stream)
+            using (var reader = new StreamReader(stream))
+            {
+                return reader.ReadToEnd();
+            }
+        }
+    }
+}
